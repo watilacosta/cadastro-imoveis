8f602f380a3141e5c95ef2bccbf904d53a03331e
watilacosta <watilafreire26@gmail.com>
2020-02-09 00:44:25 -0300

    fix assets precompile heroku

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module Intersol
     config.generators.system_tests = nil
 
     config.i18n.default_locale = :"pt-BR"
+
+    # to fix deploy on heroku
+    config.assets.initialize_on_precompile = false
   end
 end
